0220cb32f516ab7e3a48c5159ba52bc40375edd2
Dennis Zaiser <dennis.zaiser@gmail.com>
2016-05-24 00:17:03 +0200

    Initial commit

---
+++ app/src/main/java/com/zaiser/dennis/bucketdrops/ActivityMain.java
@@ -0,0 +1,13 @@
+package com.zaiser.dennis.bucketdrops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
